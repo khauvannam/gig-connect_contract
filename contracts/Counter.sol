76c235ed3eb08ae7e32053f51a6477eb2bc303e5
khauvannam <nkhauvan@gmail.com>
2025-08-25 16:42:00 +0700

    auto-script-commit

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,18 +2,25 @@
 pragma solidity ^0.8.28;
 
 contract Counter {
-  uint public x;
+    uint public x;
 
-  event Increment(uint by);
+    event Increment(uint by);
+    event Decrement(uint by);
 
-  function inc() public {
-    x++;
-    emit Increment(1);
-  }
+    function inc() public {
+        x++;
+        emit Increment(1);
+    }
 
-  function incBy(uint by) public {
-    require(by > 0, "incBy: increment should be positive");
-    x += by;
-    emit Increment(by);
-  }
+    function incBy(uint by) public {
+        require(by > 0, "incBy: increment should be positive");
+        x += by;
+        emit Increment(by);
+    }
+
+    function dec() public {
+        require(x > 0, "dec: counter should be positive");
+        x--;
+        emit Decrement(1);
+    }
 }
